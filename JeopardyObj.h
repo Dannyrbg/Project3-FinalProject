c0e41fdf0995955311b0492b195ef80557549a67
Dannyrbg <116751659+Dannyrbg@users.noreply.github.com>
2023-08-05 17:09:52 -0400

    Update JeopardyObj.h

--- JeopardyObj.h
+++ JeopardyObj.h
@@ -3,13 +3,14 @@
 #include <sstream>
 #include <vector>
 #include <string>
+#include <random>
 using namespace std;
 
+//Struct to hold each question and its inherent data
 struct questionData {
     string category;
     int value;
     string question;
     string answer;
+    bool shown = false;
 };
-
-
